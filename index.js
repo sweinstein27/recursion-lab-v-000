d297f4a339f97061556c8c782ee147e7f08a3e9c
Scott Weinstein <sweinstein27@gmail.com>
2019-05-20 20:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function reverseString(myString) {
     return reverseString(myString.substring(1)) + myString[0];
   }
 }
+
+function isPalindrome(myString) {
+  let l = myString.length;
+
+  if (l < 2) {
+    return true;
+  } else if (myString[l - 1] === myString[0]) {
+    return isPalindrome(myString.substring(1, l - 1));
+  } else {
+    return false;
+  }
+}
